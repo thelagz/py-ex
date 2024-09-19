04300631d13e6567124dd50b99fcac1bcdd5734c
Jacopo Compagnoni <illness@CYB-02.Home>
2024-09-19 13:03:25 +0200

    aggiunta commento

--- ex12.py
+++ ex12.py
@@ -1,5 +1,9 @@
+# Chiede un input e lo assegna alla variabile
 age = input("How old are you? ",)
+# Chiede un input e lo assegna alla variabile
 height = input("How tall are you? ",)
+# Chiede un input e lo assegna alla variabile
 weight = input("How much do you weight? ",)
 
+# Stampa la frase preimpostata, con i valori delle variabili assegnate
 print(f"So you are {age} old, {height} tall, and weigh {weight}")
