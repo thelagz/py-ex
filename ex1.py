912f33dff3caf9daac9362101bb4024c0e140902
Jacopo Compagnoni <illness@CYB-02.Home>
2024-09-24 22:08:35 +0200

    rinomina file

--- ex1.py
+++
@@ -1,9 +0,0 @@
-# Stampa varie stringhe
-print("Hello, World!")
-print("Hello, again!")
-print("I like typing this.")
-print("This is fun.")
-print('Yay printing.')
-print("I'd rather much you 'not'.")
-print('I "said" do not touch it.')
-print("Maronn che bell programmar")
